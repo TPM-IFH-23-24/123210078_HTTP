8f65c7b73beabe36727c13606f6f6e747a03b9f9
plirapli <rafliyulistiawan@gmail.com>
2024-04-26 21:53:38 +0700

    add ss, remove debug banner, add rmd

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Prak TPM - Latihan HTTP',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           useMaterial3: true,
         ),
